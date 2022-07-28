71170cf7e7d3f8eb710a47d136df8fd6dbf6a6dd
dankeyy <dankeyy@protonmail.com>
2022-07-28 13:41:55 +0300

    update gitignore and refactored ugly script

--- valid_il_id.py
+++ valid_il_id.py
@@ -1,15 +1,10 @@
 """Checks whether or not a given il-id is valid"""
-def valid(inp = '123456789'):
-    output = [None] * len(inp)
-    output[0::2] = [int(i) for i in inp[0::2]]
-    sum_digits = lambda x: int(str(x)[0]) + int(str(x)[1]) #get digits from a 2 digit number
 
-    for i in range(1, len(inp), 2):
-        handler = int(inp[i]) * 2
-        output[i] = sum_digits(handler) if handler > 9 else handler
+def valid(inp):
+    if not (inp.isdigit() and len(inp) == 9):
+        return False
 
-    return sum(output) % 10 == 0
-    #return output
+    out = list(map(int, inp))
+    out[1::2] = map(lambda x: sum(divmod(x * 2, 10)), out[1::2])
 
-if __name__ == '__main__':
-    print(valid(input('enter an id: ')))
+    return sum(out) % 10 == 0
